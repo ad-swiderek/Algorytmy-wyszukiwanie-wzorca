e3fad2e7da1158c1a1ab5ac54d92116fe1aee65a
ad-swiderek <37022061+Valpor@users.noreply.github.com>
2019-03-03 23:45:28 +0100

    Morris pratt

---
+++ src/KMPSearch.java
@@ -0,0 +1,10 @@
+public class KMPSearch extends StringSearch {
+    public KMPSearch(String pattern, String text) {
+        super(pattern, text);
+    }
+
+    @Override
+    public void search() {
+
+    }
+}
---
+++ src/MPSearch.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+
+public class MPSearch extends StringSearch {
+    private ArrayList<Integer> pi = new ArrayList<>();
+
+    public MPSearch(String pattern, String text) {
+        super(pattern, text);
+    }
+
+    private void fillPi() {
+        int i = 0, j = -1;
+        int m = pattern.length();
+        pi.add(-1);
+        while (i < m) {
+            while (j > -1 && pattern.charAt(i) != pattern.charAt(j)) {
+                j = pi.get(j);
+            }
+            i++;
+            j++;
+            pi.add(i, j);
+        }
+    }
+
+    @Override
+    public void search() {
+        fillPi();
+        int i = 0, j = 0;
+        int n = text.length();
+        int m = pattern.length();
+        while (i < n) {
+            while (j >= 0 && text.charAt(i) != text.charAt(j)) {
+                j = pi.get(j);
+            }
+            i++;
+            j++;
+            if (j == m) {
+                occurrences.add(i - j);
+                j = pi.get(j);
+            }
+        }
+    }
+}
